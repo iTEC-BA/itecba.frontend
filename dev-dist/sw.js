c11eb293151d21ba9e562a1f1b261be949078cea
jhon jairo pastor <jhon.jairo.tumiri@gmail.com>
2026-05-06 12:14:40 -0300

    PWA merge main

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p8ia387d9fo"
+    "revision": "0.nbtjkcq5fd"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
