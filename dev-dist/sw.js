b7a5534f0abe43ba2fb360e41e565dcc7ba1ef23
jhon jairo pastor <jhon.jairo.tumiri@gmail.com>
2026-05-09 16:09:00 -0300

    fix forum

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9d3b234'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fka07h42d9"
+    "revision": "0.v28k6f0vl28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
